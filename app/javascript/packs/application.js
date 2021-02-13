0e2d1f5db7e3597fa88136c942f976535b6f89a8
Pierre Penhard <p.penhard@gmail.com>
2021-02-13 18:54:29 +0100

    Added flatpickr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,11 +9,12 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "stylesheets/application"
-
-import { loadDynamicHeadlineText } from '../components/typed_headline';
+import { initFlatpickr } from '../plugins/init_flatpickr';
+import { loadDynamicHeadlineText } from '../plugins/typed_headline';
 
 document.addEventListener('turbolinks:load', () => {
   // loadDynamicHeadlineText();
+  initFlatpickr();
 });
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
