cf56eb38727d08fd83ac4062a4f67830d0d6968b
Pierre Penhard <p.penhard@gmail.com>
2021-02-19 13:52:36 +0100

    Added geocoder

--- app/models/appartment.rb
+++ app/models/appartment.rb
@@ -1,6 +1,7 @@
 class Appartment < ApplicationRecord
-  include PgSearch::Model
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
   def available?(from, to)
     bookings.where('start_booking <= ? AND end_booking >= ?', to, from).none?
